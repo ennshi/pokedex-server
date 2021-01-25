4fc620927f4e172f075be6fc869df256630de2df
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-25 19:56:17 +0100

    Private key fix

--- db/serviceKey.js
+++ db/serviceKey.js
@@ -2,7 +2,7 @@ module.exports = {
     "type": process.env.FS_CONFIG_TYPE,
     "project_id": process.env.FS_CONFIG_PROJECT_ID,
     "private_key_id": process.env.FS_CONFIG_PRIVATE_KEY_ID,
-    "private_key": process.env.FS_CONFIG_PRIVATE_KEY,
+    "private_key": process.env.FS_CONFIG_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": process.env.FS_CONFIG_CLIENT_EMAIL,
     "client_id": process.env.FS_CONFIG_CLIENT_ID,
     "auth_uri": process.env.FS_CONFIG_AUTH_URI,
